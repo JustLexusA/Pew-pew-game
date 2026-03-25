03e9610f82893ea14624def8fba7fff30137e373
JustAndrei <mugman121309@gmail.com>
2026-03-25 13:35:12 -0400

    ea

--- MainSketch.js
+++ MainSketch.js
@@ -1,5 +1,9 @@
 function setup() {
     createCanvas(windowWidth, windowHeight);
     background(100);
-    circle(width / 2, height / 2, 100);
+    // circle(width / 2, height / 2, 100);
+}
+
+function draw() {
+      clear();  
 }
