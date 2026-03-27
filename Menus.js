e40bdc41c56987940516054af34e662cafcdf4b9
JustAndrei <mugman121309@gmail.com>
2026-03-27 12:29:49 -0400

    added "gameplay screen and "start game" button highlight if mouse is on it

--- Menus.js
+++ Menus.js
@@ -1,11 +1,35 @@
 function startMenu() {
 	if (gameMode == 0) {
-		fill('rgb(200, 200, 200)');
+		buttonColour = color(200, 200, 200);
+		if (mouseX > windowWidth / 2 - 100 && mouseX < windowWidth / 2 + 100 &&
+			mouseY > windowHeight / 2 - 50 && mouseY < windowHeight / 2 + 50) {
+			buttonColour = color(225, 225, 225);
+		}
+		fill(buttonColour);
 		rectMode(CENTER)
 		rect(windowWidth / 2, windowHeight / 2, 200, 100);
 		fill('black')
 		textSize(20);
 		textAlign(CENTER, CENTER);
 		text("Start Game", windowWidth / 2, windowHeight / 2);
+	}
+}
+
+function mousePressed() {
+	if (gameMode == 0) {
+		if (mouseX > windowWidth / 2 - 100 && mouseX < windowWidth / 2 + 100 &&
+			mouseY > windowHeight / 2 - 50 && mouseY < windowHeight / 2 + 50) {
+			gameMode = gameplay;
+		}
+	}
+}
+
+function gameplayScreen() {
+	if (gameMode == gameplay) {
+		background(150);
+		fill('black');
+		textSize(30);
+		textAlign(CENTER, CENTER);
+		text("Gameplay Screen", windowWidth / 2, windowHeight / 2);
 	}
 }
