92f546caf3e684c4a6179da021a4f9fb16493f10
Gabriel <gabriel.theodore352@gmail.com>
2026-03-30 12:32:29 -0400

    Arena +

--- MainSketch.js
+++ MainSketch.js
@@ -2,11 +2,15 @@ function setup() {
 	createCanvas(windowWidth, windowHeight);
 	background(100);
 	gameMode = 0;
+
+	leftwall = Math.floor((windowWidth ) / 2)
+	roof = Math.floor((windowHeight ) / 2)
 }
 
 function draw() {
 	clear()
-
+	Arena()
+	
 	// Checks if the current screen should be start, gameplay, pause or shop and runs the corresponding function
 switch (gameMode) {
 	case start:
--- Variables.js
+++ Variables.js
@@ -3,3 +3,7 @@ let start = 0
 let gameplay = 1
 let pause = false
 let shop = 2
+
+//Arena
+let w = 700
+let h = 700
