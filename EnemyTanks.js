a754e1ba313560f73567fbb93a9015c60eeee9eb
Gruuben33 <ruubenpuddle@gmail.com>
2026-04-08 13:34:31 -0400

    Almost complete with making the enemy face the player, trying to fix problem where enemy does a 360 degree turn because the player moved from -180 position to 180 position

--- EnemyTanks.js
+++ EnemyTanks.js
@@ -1,19 +1,23 @@
 class enemyTank extends tank {
     constructor(color, x, y, health, size = 50) {
         super(color, x, y, health, size);
+        this.angle = 0;
         this.tankVelocity = 2*tankVelocity/3;
+        this.rotationSpeed /= 4;
     }
 
     update() {
         this.closestPlayer = this.chooseClosestPlayer()[0];
         this.closestPlayerDistance = this.chooseClosestPlayer()[1];
-        this.angleToPlayer = this.pos.angleBetween(this.closestPlayer.pos);
-        this.angleDifference = this.angleToPlayer - this.angle;
-        text(`${this.angle}`, this.pos.x, this.pos.y + 20);
-        this.angle = this.angleToPlayer;
-        if (this.angle - this.angleToPlayer > 5 || this.angle - this.angleToPlayer < -5) {
-            this.angle = this.angleToPlayer;
-        }
+        this.directionToPlayer = createVector(this.closestPlayer.pos.x - this.pos.x, this.closestPlayer.pos.y - this.pos.y);
+        this.targetAngle = this.directionToPlayer.heading() - 90;
+        text(`${this.targetAngle}`, this.pos.x, this.pos.y + 20);
+        if (this.targetAngle - this.angle < 2) {
+            text(`${this.targetAngle}`, this.pos.x, this.pos.y + 20);
+            this.angle += Math.sign(this.targetAngle - this.angle) * this.rotationSpeed;
+        }// else if (this.targetAngle - (this.angle - 90) > -2) {
+        //    this.angle += this.rotationSpeed;
+        //}
     }
 
     chooseClosestPlayer() {
